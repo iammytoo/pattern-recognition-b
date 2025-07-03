20f735f690c1a99c13532273df87b7c25a521649
Taiki <tacosuke12@gmail.com>
2025-07-04 00:54:44 +0900

    cross_encoderのloraの修正

--- src/run/cross_encoder/lora_cross_encoder.py
+++ src/run/cross_encoder/lora_cross_encoder.py
@@ -15,16 +15,21 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 def main():
     """ メインメソッド """
     # データの取得
-    data_file_path = "data/caption_processed/train.csv"
-    train_df = pd.read_csv(data_file_path)
+    train_file_path = "data/caption_processed/train.csv"
+    val_file_path   = "data/caption_processed/validation.csv"
+
+    train_df = pd.read_csv(train_file_path)
+    val_df   = pd.read_csv(val_file_path)
 
     train_df['score'] = (train_df['score'] - train_df['score'].min()) / (train_df['score'].max() - train_df['score'].min())
+    val_df['score']   = (val_df['score']   - val_df['score'].min())   / (val_df['score'].max()   - val_df['score'].min()) 
 
     train_dataset = Dataset.from_pandas(train_df)
+    val_dataset   = Dataset.from_pandas(val_df)
 
     # ファインチューニング
     reranker = RerankerCrossEncoderClient()
-    reranker.train_with_lora(train_dataset=train_dataset)
+    reranker.train_with_lora(train_dataset, val_dataset)
 
 
 if __name__ == "__main__":
